47395547a308f07df0233712f65f86ab6972b354
adndax <adindaputri0517@gmail.com>
2025-05-05 21:38:40 +0700

    feat: add data

--- data/data.tsx
+++ data/data.tsx
@@ -1,1 +1,176 @@
 export const characters = ["berserker", "mage", "fighter", "necromancer", "assassin"];
+
+export const skills = {
+
+  // Mage
+  "curse-of-crow": {
+    name: "Curse of Crow",
+    description:
+      'Noelle adalah seorang gadis yang tinggal di desa Futuarte, sebuah daerah di negeri Blourta pada zaman Xantorice. Ia hidup sebatang kara, kedua orang tuanya meninggal disaat umurnya 3 tahun. Dalam kesehariannya, ia hanya ditemani oleh satu burung gagak, bernama Secre. Kemanapun Noelle pergi, Secre selalu hinggap di atas pundak kanannya. Secre sering membuat kegaduhan di tengah desa Futuarte. Akibat dari kejadian ini, banyak orang mengumpat kepada Noelle dan burungnya, dan memanggilnya dengan julukan “Curse Of Crow”.',
+  },
+  "black-shoes": {
+    name: "Black Shoes",
+    description:
+      'Kemanapun Noelle pergi, ia selalu mengenakan sepatu hitam yang dimilikinya. Sepatu kusut tersebut sudah menempel di kakinya selama lebih dari satu tahun. Pada suatu pagi yang cerah, Noelle memutuskan untuk melepas sepatu tersebut karena merasa pegal. Ia meletakkannya di depan rumahnya dan pergi tidur. Namun, saat ia terbangun, Noelle terkejut karena banyak barang berharga miliknya yang hilang, termasuk sepatu kesayangannya. Secre memberitahunya bahwa sekelompok orang dewasa diam-diam masuk ke rumah dan mencuri beberapa barang. Secre tahu arah perginya para pencuri tersebut. Dengan penuh amarah, Noelle bertekad mengejar para pencuri itu, membawa tongkat sihir di tangannya.',
+  },
+  "poker-face": {
+    name: "Poker Face",
+    description:
+      'Setelah kehilangan sepatu hitam kesayangannya dan menyadari bahwa tak ada seorang pun yang benar-benar peduli padanya, Noelle mulai berubah. Ia menanggalkan sisi emosional yang dulu membuatnya tampak lemah dan mulai membangun tembok dingin dalam dirinya. Wajahnya kini selalu datar, tak menunjukkan sedikit pun perasaan—entah itu saat bertemu dengan orang yang mencibirnya, atau saat menatap jejak pencuri yang telah menghancurkan satu-satunya tempat yang ia sebut rumah. Ia belajar menyembunyikan emosinya sebagai bentuk perlindungan diri, dan sejak saat itu, penduduk desa mulai menyebutnya dengan julukan baru: “Poker Face”.',
+  },
+  "ever-play-this-game-before": {
+    name: "Ever Play This Game Before?",
+    description:
+      "Skill psikologis yang membuat musuh meragukan realitas. Cocok digunakan untuk mengacaukan strategi lawan.",
+  },
+  "avada-kedavra": {
+    name: "Avada Kedavra",
+    description:
+      "Kutukan maut kuno. Sekali terucap, kematian datang tanpa ampun. Hanya bisa digunakan oleh mereka yang siap kehilangan segalanya.",
+  },
+  "one-day-or-day-one": {
+    name: "One Day or Day One",
+    description:
+      'Namun, di balik ketenangannya yang mencekam, Noelle menyimpan perhitungan tajam dan siasat yang penuh perhitungan. Wajahnya yang tak tergoyahkan menjadi senjata tersendiri dalam menghadapi musuh. Ia mampu membaca lawan tanpa membiarkan pikirannya terbaca. Dalam setiap pertempuran, ekspresi kosong itu membuat lawan ragu dan tidak siap menghadapi langkah berikutnya. Dengan sihir dan strategi yang tak tertebak, Noelle menjelma menjadi sosok yang bukan hanya ditakuti karena kekuatannya, tapi juga karena misteri di balik wajah yang tak pernah berubah.',
+  },
+
+  // Fighter
+  "dont-run": {
+    name: "Don't Run",
+    description:
+      "Pada masa Xantonice, hidup seorang petinju yang lemah dan selalu menelan kekalahan di setiap pertandingan. Rekornya begitu buruk hingga hampir mencapai angka tiga digit kekalahan. Karena kegagalannya itu, banyak orang menjauhinya, bahkan anak-anak muda sering merundungnya. Suatu malam yang kelam, saat ia melintas di sebuah gang sempit, ia berpapasan dengan sepuluh orang jahat yang berniat menculik dan menghabisinya. Menyadari betapa kecil peluangnya untuk melawan, ia memilih untuk melarikan diri. Namun, sang pemimpin geng sang menyeru petinju dengan satu seruan: “Dont' Run!”",
+  },
+  "heavy-left-punch": {
+    name: "Heavy Left Punch",
+    description:
+      "Petinju itu akhirnya mendapati dirinya terjebak, dikepung dari segala penjuru tanpa satu pun jalan keluar. Dalam keputusasaan, ia hampir sepenuhnya menyerah, bahkan tak lagi berniat melawan. Namun, ketika para anggota geng mulai menyerbunya, sebuah refleks tak terduga menggerakkan tubuhnya. Dengan gerakan spontan, tangan kirinya meluncur ke depan, menghantam salah satu penyerangnya dengan pukulan keras. Heavy Left Punch menghantam begitu kuat hingga menyebabkan salah satu anggota geng kehilangan fungsi penglihatannya.",
+  },
+  "black-dragon-form": {
+    name: "Black Dragon Form",
+    description:
+      "Akibat peristiwa itu, sebuah aura gelap meledak dari dalam dirinya, membuat kekuatannya melonjak drastis. Wujud Black Dragon Form membangkitkan kemampuan bertinju luar biasa dalam dirinya, menjadikannya petarung yang tak tertandingi. Tanpa membuang waktu, ia segera melancarkan serangan demi serangan, menghabisi seluruh musuh yang mengelilinginya hingga tak satu pun yang tersisa.",
+  },
+  "divine-judgment": {
+    name: "Divine Judgment",
+    description:
+      "Petinju itu akhirnya terpojok, dikepung dari segala arah tanpa secercah jalan keluar. Dalam keputusasaan, semangatnya nyaris padam, dan ia hampir tak lagi memiliki niat untuk melawan. Namun saat geng itu mulai menyerbunya, sebuah pesan dari orang tuanya terlintas di benaknya — untuk selalu memohon pertolongan kepada dewa saat segala jalan telah tertutup. Di saat itu juga, seberkas petir menyambar dari langit, sebagai Divine Judgment yang dahsyat, menghantam empat anggota geng sekaligus hingga tubuh mereka terbakar.",
+  },
+  "never-coming-back": {
+    name: "Never Coming Back",
+    description:
+      'Terkejut oleh kejadian itu, enam anggota geng yang tersisa langsung melarikan diri, ketakutan. Sang petinju, masih diliputi keterkejutan atas apa yang baru saja terjadi, hanya bisa terpaku. Namun, tak lama kemudian, senyum licik mulai terukir di wajahnya — senyum seorang villain yang berdiri di atas tubuh musuh-musuhnya yang telah tumbang. Dengan suara dingin, ia berbisik, "Never Coming Back."',
+  },
+  "the-day-has-come": {
+    name: "The Day Has Come",
+    description:
+      "Skill pamungkas. Menggabungkan semua kekuatan sebelumnya dalam satu pukulan penentu nasib.",
+  },
+
+  // Berseker
+   "huh": {
+    name: "Huh",
+    description:
+    'Saat desa kecilnya dibakar habis oleh bangsa Viking, bocah itu hanya berdiri mematung di tengah puing-puing. Ayahnya tewas. Ibunya hilang. Ia ditangkap dan dibawa ke utara, jauh dari tanah kelahirannya. Dia tidak menangis. Tidak menjerit. Dia hanya berkata satu hal… “Huh?” Itu bukan kebingungan. Itu kekosongan. Dan dari kekosongan itu, sesuatu mulai tumbuh.',
+  },
+  "i-give-up": {
+    name: "I Give Up",
+    description:
+      "Di cabang berbeda, saat dia lebih muda dan belum tahu apa-apa… Dia pernah mencoba melarikan diri dari perkemahan Viking. Dia gagal. Dihajar sampai nyaris mati, tubuhnya dipenuhi luka. Di malam itu, dia menatap langit. “I give up…” Tapi tubuhnya tetap hidup. Napasnya tetap ada. Dan itu awal dari tubuhnya belajar satu hal: Bahkan menyerah pun tidak menyelamatkanmu.",
+  },
+  "fun-i-guess": {
+    name: "Fun, I Guess?",
+    description:
+      'Tahun berlalu. Di tengah kamp pelatihan, anak itu diajari bertarung. Melempar kapak. Membelah perisai. Menahan rasa sakit. Awalnya dia tidak mengerti mengapa orang tertawa setelah menebas musuh. Tapi saat dia membuat darah pertama tumpah… Ada sesuatu yang berdesir di dadanya. Itu bukan kebanggaan. Bukan kemenangan. Mungkin… “Fun, I guess?”. Dan senyuman kecil mulai muncul saat dia menghancurkan hidung lawan latihannya.',
+  },
+  "had-no-chance": {
+    name: "Had No Chance",
+    description:
+      'Dalam pertempuran di musim salju, dia terluka parah. Semua teman satu klannya mati. Dia sendiri. Dikelilingi. Pedang dan tombak mengarah padanya dari segala sisi. Dan semua orang berpikir: “Dia tidak punya peluang.” Tapi tubuhnya bergerak sendiri. Pikiran kosong. Tangannya memotong, menebas, menghancurkan. Hingga tidak ada yang tersisa kecuali darah dan bangkai. “I… Had no chance,” katanya sambil tertawa histeris, tubuhnya bersimbah darah.',
+  },
+  "taste-of-killing": {
+    name: "Taste of Killing",
+    description:
+      'Sekarang dia bukan anak-anak lagi. Dia tak lagi berlatih di arena, tapi di ladang pertempuran. Musuh pertamanya? Prajurit dari tanah yang dulu ia sebut rumah. Ketika ia menebas leher mereka, ia tidak gemetar. Sebaliknya, ia menghirup aroma darah seperti parfum mahal. “Rasanya… pas.” Taste of Killing membuka sisi gelap yang tertidur lama dalam dirinya—dan kini, ia tidak ingin menghentikannya.',
+  },
+  "meet-you-at-hell": {
+    name: "Meet You at Hell",
+    description:
+      'Berserker itu kini menjadi legenda. Lawan yang melihatnya langsung lari atau berdoa. Tapi bagi dia, semua sudah terlalu tenang. Jadi dia berteriak di medan perang, “AKU AKAN MENUNGGU KALIAN DI NERAKA!!” Dan dengan setiap kill, dia merasa lebih dekat ke tempat itu. Bukan karena takut… Tapi karena itu rumahnya sekarang. Meet You at Hell adalah akhir dari perjalanan pembantaian—di mana semua kematian menjadi janji pertemuan.',
+  },
+
+// Necromancer
+"go-out-son": {
+    name: "Go Out Son!",
+    description:
+      "Racun itu mulai menyebar. Putri Elzara tahu waktunya sudah hampir habis sebagai manusia. Sebelum ambruk, dia menggenggam tangan putranya yang masih kecil dan berbisik: “Pergilah, Nak. Jangan lihat ibu lagi.” Itulah keputusan terakhir yang ia buat sebagai Elzara sang putri. Dan saat itu juga… Necromancer lahir.",
+  },
+  "survive-please": {
+    name: "Survive, Please!",
+    description:
+      "Tubuhnya membusuk namun hidup. Jiwanya terkoyak, namun kuat. Dalam insting terakhirnya sebagai seorang ibu, Elzara menciptakan mantra pelindung yang menjaga jalan kabur anaknya. Satu-satunya perintah dalam pikirannya saat itu adalah: “Bertahanlah…” Namun mantra itu juga memanggil tulang-tulang kurcaci leluhur. Mereka bangkit. Dan Elzara tidak bisa lagi membedakan antara melindungi… dan membalas dendam.",
+  },
+  "to-easy": {
+    name: "Too Easy",
+    description:
+      "Di jalur berbeda, Elzara tidak langsung berhenti. Setiap pertempuran hanya terasa seperti permainan. Tak ada strategi. Tak ada kesulitan. Musuhnya datang, dan langsung roboh. Setiap mantra adalah siksa. Setiap gerakan adalah eksekusi. “Mudah sekali,” katanya datar, sambil mengubah pasukan elit Solmark menjadi boneka tulang. Ini bukan perang. Ini ekshibisi.",
+  },
+  "final-fatality": {
+    name: "Final Fatality",
+    description:
+      "Di titik ini, Elzara kehilangan kendali. Ia menciptakan mantra terakhir—kutukan purba yang meledakkan seluruh daratan Solmark. Gunung runtuh. Laut mendidih. Kota-kota musnah dalam hitungan jam. Teriakan terakhir dunia manusia bergema di udara. Ia berdiri di atas dunia yang mati. Elzara pun membatu, menjadi patung obsidian di tengah kehancuran yang ia buat sendiri.",
+  },
+  "fine-ill-stop": {
+    name: "Fine I’ll Stop",
+    description:
+      "Pertempuran melawan Kerajaan Solmark berlangsung selama satu dekade. Raja Solmark telah lama mati. Para pewarisnya pun sudah tiada. Pasukan musuh runtuh, rakyat mereka pun bertekuk lutut. Elzara berdiri di atas menara ibukota, melihat dunia tanpa warna. Semua musuh telah habis. Tidak ada yang tersisa. Ia membuka tangannya… dan pasukan kurcaci mayat hidup mulai beristirahat satu per satu. “Baiklah. Aku berhenti,” gumamnya, sendirian dalam kemenangan yang terasa kosong. Fine, I’ll Stop bukan karena lelah, tapi karena tidak ada lagi yang layak dihancurkan.",
+  },
+  "punish-them": {
+    name: "Punish Them",
+    description:
+      "Dia tidak ingin dunia lenyap… dia ingin dunia merasakan. Setiap makhluk hidup menjadi bahan eksperimen. Ia menciptakan siklus hidup dan mati yang menyakitkan, di mana korban dibangkitkan hanya untuk dibunuh lagi. Penderitaan tanpa akhir. Siksaan yang tak bisa dijelaskan oleh waktu atau logika. “Hukuman kalian… baru saja dimulai.” PUNISH THEM adalah bentuk terdistorsi dari keadilan seorang ibu yang kehilangan segalanya.",
+  },
+
+// Assassin
+"blade-storm": {
+    name: "Blade Storm",
+    description:
+      'Marsian, anak seorang sultan yang sedang berangkat ke negeri white elf  menggunakan kapal pesiar. Di Tengah perjalanan, Badai menerjang kapalnya. Kapal tersebut terbalikkan oleh ombak, kemudian terhancurkan oleh batu besar glitzzberg. Ia berhasil selamat, namun terdampar disebuah negeri reginbombom, sebuah negeri yang dihuni bangsa regin, bangsa yang terkenal akan sifatnya yang bengis dan pengrusak. Ia tertangkap kemudian disiksa oleh bangsa tersebut tiap hari selama 99 hari. Di hari ke 100, amarahnya yang terkumpul selama 99 hari terkeluarkan, dan BOOM “Blade Storm” menghancurkan satu peleton bangsa regin. HAHAHAAHA , mampus kalian para reginn - ucap marsian.',
+  },
+  "shadow-kill": {
+    name: "Shadow Kill",
+    description:
+      "Dengan Shadow Kill, dia tidak lagi membantai dengan teriakan , dia membunuh dengan keheningan yang menyayat jiwa. Dia merayap lewat retakan lantai, menetes dari bayangan lilin, bahkan muncul dari refleksi darah yang menggenang. Tidak ada suara. Tidak ada peringatan… boom mati kabeh.",
+  },
+  "play-till-dead": {
+    name: "Play Till Dead",
+    description:
+      "Kegilaan akan darah merasuki marsian. Tiap membunuh ia tertawa layaknya seorang anak yang mendapat mainan, namun mainannya adalah organ musuhnya, yakni bangsa regin. HAHAHAAH organ mereka enak juga - ucap marsian. kill ini bukan soal bertahan hidup. Ini soal main sampai semua mainan rusak, A.KA organ para musuhnya. Setiap kill memberinya dorongan dopamin yang tidak masuk akal. Dengan Play Til Dead, dia makin kuat bukan karena strategi... tapi karena otaknya tidak punya rem. Ia akan terus bertarung, bukan sampai musuh mati, tapi sampai tubuhnya sendiri hancur tak bisa berdiri lagi dan bahkan saat itu pun, dia masih tersenyum.",
+  },
+  "bad-dreams": {
+    name: "Bad Dreams",
+    description:
+      'Para prajurit regin yang selamat, gemetar dalam barak, saling menjaga agar tidak tertidur. Karena jika tertidur, mereka mungkin mendapat “bad dreams”, namun ini bukan mimpi buruk biasa…Mimpi ini bisa membuatmu mati seketika secara harfiah.. Marsian, he is a bad dream.',
+  },
+  "assassins-pride": {
+    name: "Assassin’s Pride",
+    description:
+      'Dengan penuh kebanggan, marsian mebantai para pasukan dengan kekuatan yang penuh ketangkasan. Ia meluncur seperti panah, memantul dari satu musuh ke musuh lain, membunuh dalam gerakan halus tanpa sia-sia. Ia membunuh dengan penuh rasa bangga. Ini bukan kebanggaan biasa. Ini kebanggaan seorang dewa pembunuh yang lahir dari 99 hari siksaan.',
+  },
+  "clutch-in-the-gene": {
+    name: "Clutch in the Gene",
+    description:
+      "Ada hal yang bangsa Regin tidak tahu saat mereka menyiksa Marsian. Mereka kira dia manusia biasa. Mereka kira dia lemah. Mereka salah besar. Di dalam darah Marsian mengalir sesuatu yang bahkan ia sendiri tak pahami. Gen pemburu. Gen pembantai. Gen yang hanya aktif saat titik balik antara hidup dan mati terlewati.  Clutch in the Gene merupakan mode penghancuran otomatis, di mana Marsian kehilangan kontrol... tapi tubuhnya tahu persis apa yang harus dilakukan, yakni MEMBANTAIII.",
+  },
+  "taste-of-blood": {
+    name: "Taste of Blood",
+    description:
+      'Marsian dulu membunuh karena dendam. Tapi setelah puluhan, ratusan, bahkan ribuan darah Regin mengotori tangannya… dia mulai menikmatinya. Rasa darah mereka di udara. Di lidah. Di pikirannya. Itu bukan hanya balas dendam lagi. Itu adiksi. Dengan Taste of Blood, setiap tetes darah musuh yang menyentuh kulitnya membuat tubuhnya lebih kuat, lebih cepat, lebih haus. Jantungnya berdetak seperti genderang perang, dan matanya membara seperti bara dalam malam yang gelap. Dia tidak hanya mengejar musuhnya. Dia memburu mereka. “Rasa darah kalian… lebih nikmat dari anggur kerajaan,” bisiknya saat mencabik pasukan Regin satu per satu. Dan saat darah terakhir menetes, ia menjilat jarinya, tersenyum lebar. “Enak.”',
+  },
+  "your-time-has-come": {
+    name: "Your Time Has Come",
+    description:
+      'Tidak ada lagi pertanyaan. Tidak ada lagi ampun. Tidak ada lagi negosiasi. Setelah 99 hari disiksa dan bertahan, Marsian tak lagi manusia. Ia menjadi eksekutor takdir. Dengan Your Time Has Come, setiap langkahnya adalah vonis mati. Satu serangan. Satu gerakan. Satu tarikan napas. Dan musuh hilang. Tubuh musuhnya tak sempat jatuh. Bayangan Marsian sudah hilang sebelum darah menyentuh tanah. “Apakah itu pedang?”, “Tidak. Itu hanya kematian.”, Regin akhirnya sadar… Mereka tidak sedang melawan Marsian. Mereka sedang dihukum oleh sesuatu yang tak bisa mereka mengerti. Sesuatu yang hanya bisa berkata satu hal sebelum memulai: “Your time has come.”',
+  },
+} as const;
+
+export type SkillId = keyof typeof skills;
