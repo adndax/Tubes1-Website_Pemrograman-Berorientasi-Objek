1b7e4a047f7b249d9d55eba36cd7bd4e94f3ca29
adndax <adindaputri0517@gmail.com>
2025-05-05 20:22:25 +0700

    fix: character link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
+import { characters } from "../../data/data";
+
 
 export default function Home() {
+
   return (
     <div className="min-h-screen bg-[var(--background)] text-[var(--foreground)] flex flex-col items-center justify-start px-4 py-30 gap-8">
       <Image
@@ -18,23 +22,21 @@ export default function Home() {
       <p className="font-[var(--font-pixelify)] text-xs sm:text-sm leading-relaxed max-w-3xl text-center">
         Dalam dunia yang dipenuhi darah, sihir, dan pengkhianatan, tidak ada kekuatan yang muncul begitu saja.
         Setiap skill adalah jejak langkah, setiap pilihan adalah luka, dan setiap akhir… adalah warisan dari masa lalu.
-
-        <br/><br/>
+        <br /><br />
         Telusuri sejarah mereka – para Fighter, Mage, Assassin, Berserker, dan Necromancer – dan pahami bahwa di balik setiap kekuatan...
         Tersembunyi cerita yang belum selesai.
       </p>
 
       <div className="flex flex-wrap justify-center gap-4 mt-6">
-        {/* Kartu karakter */}
-        {["berserker", "mage", "fighter", "necromancer", "assassin"].map((char) => (
-          <div key={char} className="flex flex-col items-center">
+        {characters.map((char) => (
+          <Link key={char} href={`/${char}`} className="flex flex-col items-center cursor-pointer hover:scale-105 transition-transform">
             <Image
-              src={`/${char}.png`} // contoh: public/characters/berserker.png
+              src={`/${char}.png`}
               alt={char}
               width={150}
               height={150}
             />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
